170d6650bebcf0861dcf16ff241221ed27fc7724
Eduardo Guimaraes <eduguimara@gmail.com>
2021-08-09 16:29:38 -0300

    select one, update one and batch update rides

--- src/main/java/com/pluralsight/service/RideServiceImpl.java
+++ src/main/java/com/pluralsight/service/RideServiceImpl.java
@@ -1,5 +1,7 @@
 package com.pluralsight.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,4 +25,29 @@ public class RideServiceImpl implements RideService {
 	public Ride createRide(Ride ride) {
 		return rideRepository.createRide(ride);
 	}
+	
+	@Override
+	public Ride getRide(Integer id) {
+		return rideRepository.getRide(id);
+	}
+
+	@Override
+	public Ride updateRide(Ride ride) {
+		return rideRepository.updateRide(ride);
+	}
+
+	@Override
+	public void batch() {
+		List<Ride> rides = rideRepository.getRides();
+		
+		List<Object[]> pairs = new ArrayList<>();
+		
+		for (Ride ride : rides) {
+			Object[] temp = {new Date(), ride.getId()};
+			pairs.add(temp);
+		}
+		
+		rideRepository.updateRides(pairs);
+	}
+	
 }
